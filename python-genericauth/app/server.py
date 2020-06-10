9ecb0a3af77b0f2494c259006d5dfd6088729aa2
xdassie <50299712+xdassie@users.noreply.github.com>
2020-06-10 15:53:27 +0200

    Update server.py

--- python-genericauth/app/server.py
+++ python-genericauth/app/server.py
@@ -99,7 +99,7 @@ def index():
 #    return Response(response="", status=403,mimetype="application/json")
     authorization_header = request.headers.get('Authorization')
     if authorization_header and check(authorization_header):
-        return Response(response="", status=200, mimetype="application/json")
+        return Response(response="{}", status=200, mimetype="application/json")
     else:
         resp = Response()
         resp.headers['WWW-Authenticate'] = 'Basic'
