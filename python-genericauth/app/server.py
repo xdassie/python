42d591aeeb4214a1cb6566a685d328df94e4b1f3
xdassie <50299712+xdassie@users.noreply.github.com>
2020-06-10 18:41:42 +0200

    Update server.py

--- python-genericauth/app/server.py
+++ python-genericauth/app/server.py
@@ -98,8 +98,8 @@ def index():
 #    return Response(response="{}", status=200, mimetype="application/json")
 #    return Response(response="", status=403,mimetype="application/json")
     authorization_header = request.headers.get('Authorization')
-#    if check(authorization_header) == True:
-    if 1 == 1:
+    result = check(authorization_header)
+    if result == True:
         return Response(response="{}", status=200, mimetype="application/json")
     else:
         resp = Response()
